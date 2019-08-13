e0031666783611c629378774a20e5e522b6c1715
dzhynenko-pinedev <50555089+dzhynenko-pinedev@users.noreply.github.com>
2019-08-13 13:23:06 +0300

    Update EntityWasDeleted.php

--- src/Events/EntityWasDeleted.php
+++ src/Events/EntityWasDeleted.php
@@ -19,21 +19,22 @@ class EntityWasDeleted
      */
     public function handle(Entity $entity): void
     {
+        return;
         // We will initially check if the model is using soft deletes. If so,
         // the attribute values will remain untouched as they should sill
         // be available till the entity is truly deleted from database.
-        if (in_array(SoftDeletes::class, class_uses_recursive(get_class($entity))) && ! $entity->isForceDeleting()) {
-            return;
-        }
+//         if (in_array(SoftDeletes::class, class_uses_recursive(get_class($entity))) && ! $entity->isForceDeleting()) {
+//             return;
+//         }
 
-        foreach ($entity->getEntityAttributes() as $attribute) {
-            if ($entity->relationLoaded($relation = $attribute->getAttribute('slug'))
-                && ($values = $entity->getRelationValue($relation)) && ! $values->isEmpty()) {
+//         foreach ($entity->getEntityAttributes() as $attribute) {
+//             if ($entity->relationLoaded($relation = $attribute->getAttribute('slug'))
+//                 && ($values = $entity->getRelationValue($relation)) && ! $values->isEmpty()) {
                 // Calling the `destroy` method from the given $type model class name
                 // will finally delete the records from database if any was found.
                 // We'll just provide an array containing the ids to be deleted.
-                forward_static_call_array([Attribute::getTypeModel($attribute->getAttribute('type')), 'destroy'], [$values->pluck('id')->toArray()]);
-            }
-        }
+//                 forward_static_call_array([Attribute::getTypeModel($attribute->getAttribute('type')), 'destroy'], [$values->pluck('id')->toArray()]);
+//             }
+//         }
     }
 }
